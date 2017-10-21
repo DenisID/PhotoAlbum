1d19894c0a6c26b1afbde7d5d2bff8b95ce7a17a
DenisID <f40f41@gmail.com>
2017-10-21 18:49:15 +0700

    PA.C.Model : add PhotoAlbumService class (empty)

---
+++ PhotoAlbum.Client.Model/Services/PhotoAlbumService.cs
@@ -0,0 +1,14 @@
+﻿using PhotoAlbum.Client.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Client.Model.Services
+{
+    class PhotoAlbumService : IPhotoAlbumService
+    {
+
+    }
+}
