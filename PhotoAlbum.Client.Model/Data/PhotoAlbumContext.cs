4b2b245e388a67c198a37b49669e065079bf09de
DenisID <f40f41@gmail.com>
2017-10-21 17:29:26 +0700

    PA.C.Model : add PhotoAlbumContext

---
+++ PhotoAlbum.Client.Model/Data/PhotoAlbumContext.cs
@@ -0,0 +1,24 @@
+﻿using PhotoAlbum.Client.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Client.Model.Data
+{
+    class PhotoAlbumContext : DbContext
+    {
+        public PhotoAlbumContext()
+            : base("name=DefaultConnection")
+        {
+            Database.SetInitializer(new CreateDatabaseIfNotExists<PhotoAlbumContext>());
+        }
+
+        public DbSet<Photo> Photos { get; set; }
+        public DbSet<PhotoContent> PhotoContents { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<PhotoVote> PhotoVotes { get; set; }
+    }
+}
