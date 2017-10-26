8beba1e6ded288fb59a0a4dad346a3ae2f302f0c
DenisID <f40f41@gmail.com>
2017-10-26 19:39:29 +0700

    pa.s.Model : add PhotoAlbumService (e)

---
+++ PhotoAlbum.Server.Model/Services/PhotoAlbumService.cs
@@ -0,0 +1,13 @@
+﻿using PhotoAlbum.Server.Model.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Server.Model.Services
+{
+    class PhotoAlbumService : IPhotoAlbumService
+    {
+    }
+}
