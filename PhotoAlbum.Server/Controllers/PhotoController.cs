96bc8bb748c30d736b08c0de3a9024005c7b0ec0
DenisID <f40f41@gmail.com>
2017-10-27 12:01:06 +0700

    wip

--- PhotoAlbum.Server/Controllers/PhotoController.cs
+++ PhotoAlbum.Server/Controllers/PhotoController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PhotoAlbum.Server.Dto;
+using PhotoAlbum.Server.Model.Interfaces;
+using PhotoAlbum.Server.Model.Services;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +12,52 @@ namespace PhotoAlbum.Server.Controllers
 {
     public class PhotoController : ApiController
     {
+        private IPhotoAlbumService _photoAlbumService = new PhotoAlbumService();
+
+        // GET api/values
+        public IEnumerable<string> GetAllPhoto()
+        {         
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/photo
+        public void PostPhoto([FromBody]AddPhotoDto addPhotoDto)
+        {
+            if (ModelState.IsValid && addPhotoDto.Image != null)
+            {
+                byte[] imageData = null;
+                // считываем переданный файл в массив байтов
+                using (var binaryReader = new BinaryReader(addPhotoModel.Image.InputStream))
+                {
+                    imageData = binaryReader.ReadBytes(addPhotoModel.Image.ContentLength);
+                }
+                // установка массива байтов
+                var addPhotoDto = new AddPhotoDto();
+                addPhotoDto.Image = imageData;
+                addPhotoDto.Title = addPhotoModel.Title;
+                addPhotoDto.Description = addPhotoModel.Description;
+
+                _photoAlbumService.AddPhoto(addPhotoDto);
+
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // PUT api/values/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
     }
 }
