b2319ccdf4bd41cc284c2e60d1e3f9f9e706e4ab
DenisID <f40f41@gmail.com>
2017-10-28 17:25:37 +0700

    pa.c.BusinessServices : add PhotoAlbumService interface (e) and class (e)

---
+++ PhotoAlbum.Client.BusinessServices/Services/PhotoAlbumService.cs
@@ -0,0 +1,13 @@
+﻿using PhotoAlbum.Client.BusinessServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Client.BusinessServices.Services
+{
+    public class PhotoAlbumService : IPhotoAlbumService
+    {
+    }
+}
