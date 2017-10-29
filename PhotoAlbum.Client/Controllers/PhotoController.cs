cf404331fc808ad97195d80e9deb47d0f226d5e2
DenisID <f40f41@gmail.com>
2017-10-29 12:48:02 +0700

    pa.c PhotoController : Test(), Index(), CreatePhoto(), [HttpPost] CreatePhoto()

--- PhotoAlbum.Client/Controllers/PhotoController.cs
+++ PhotoAlbum.Client/Controllers/PhotoController.cs
@@ -1,10 +1,14 @@
-﻿using PhotoAlbum.Client.Dto;
-using PhotoAlbum.Client.Model.Services;
+﻿using PhotoAlbum.Client.BusinessServices.Interfaces;
+using PhotoAlbum.Client.BusinessServices.Services;
+using PhotoAlbum.Client.Dto;
+//using PhotoAlbum.Client.Model.Services;
 using PhotoAlbum.Client.Models;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,47 +16,83 @@ namespace PhotoAlbum.Client.Controllers
 {
     public class PhotoController : Controller
     {
-        private PhotoAlbumService _photoAlbumService = new PhotoAlbumService();
+        //private PhotoAlbumService _photoAlbumService = new PhotoAlbumService();
+        private IPhotoAlbumService _photoAlbumService = new PhotoAlbumService();
+
+        public async Task<HttpResponseMessage> Test()
+        {
+            var response = await _photoAlbumService.Test();
+            var result = response.Content;
+            
+            return response;
+        }
 
         // GET: Photo
         public ActionResult Index()
         {
-            var photos = _photoAlbumService.GetAllPhotos();
+            //var photos = _photoAlbumService.GetAllPhotos();
+            List<PhotoModel> photos = null;
             return View(photos);
         }
 
-        public ActionResult AddPhoto()
+        public ActionResult CreatePhoto()
         {
             return View();
         }
 
         [HttpPost]
-        public ActionResult AddPhoto(AddPhotoModel addPhotoModel)
+        public async Task<ActionResult> CreatePhoto(CreatePhotoModel createPhotoModel)
         {
-            if (ModelState.IsValid && addPhotoModel.Image != null)
+            if (ModelState.IsValid && createPhotoModel.Image != null)
             {
                 byte[] imageData = null;
                 // считываем переданный файл в массив байтов
-                using (var binaryReader = new BinaryReader(addPhotoModel.Image.InputStream))
+                using (var binaryReader = new BinaryReader(createPhotoModel.Image.InputStream))
                 {
-                    imageData = binaryReader.ReadBytes(addPhotoModel.Image.ContentLength);
+                    imageData = binaryReader.ReadBytes(createPhotoModel.Image.ContentLength);
                 }
                 // установка массива байтов
-                var addPhotoDto = new AddPhotoDto();
-                addPhotoDto.Image = imageData;
-                addPhotoDto.Title = addPhotoModel.Title;
-                addPhotoDto.Description = addPhotoModel.Description;
+                var createPhotoDto = new CreatePhotoDto();
+                createPhotoDto.Image = imageData;
+                createPhotoDto.Title = createPhotoModel.Title;
+                createPhotoDto.Description = createPhotoModel.Description;
 
-                _photoAlbumService.AddPhoto(addPhotoDto);
+                await _photoAlbumService.CreatePhoto(createPhotoDto);
 
                 return RedirectToAction("Index");
             }
             return View();
         }
 
-        public void Delete(int photoId)
-        {
-            _photoAlbumService.DeletePhoto(photoId);
-        }
+
+
+        //[HttpPost]
+        //public ActionResult AddPhoto(AddPhotoModel addPhotoModel)
+        //{
+        //    if (ModelState.IsValid && addPhotoModel.Image != null)
+        //    {
+        //        byte[] imageData = null;
+        //        // считываем переданный файл в массив байтов
+        //        using (var binaryReader = new BinaryReader(addPhotoModel.Image.InputStream))
+        //        {
+        //            imageData = binaryReader.ReadBytes(addPhotoModel.Image.ContentLength);
+        //        }
+        //        // установка массива байтов
+        //        var addPhotoDto = new AddPhotoDto();
+        //        addPhotoDto.Image = imageData;
+        //        addPhotoDto.Title = addPhotoModel.Title;
+        //        addPhotoDto.Description = addPhotoModel.Description;
+
+        //        _photoAlbumService.AddPhoto(addPhotoDto);
+
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View();
+        //}
+
+        //public void Delete(int photoId)
+        //{
+        //    _photoAlbumService.DeletePhoto(photoId);
+        //}
     }
 }
