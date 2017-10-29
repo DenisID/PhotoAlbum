cdb858bb6d672786b5ffcde22569b88880bbef02
DenisID <f40f41@gmail.com>
2017-10-29 12:54:26 +0700

    pa.s.Dto : add CreatePhotoDto class

---
+++ PhotoAlbum.Server.Dto/CreatePhotoDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Server.Dto
+{
+    class CreatePhotoDto
+    {
+        public byte[] Image { get; set; }
+        public string ImageMimeType { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
