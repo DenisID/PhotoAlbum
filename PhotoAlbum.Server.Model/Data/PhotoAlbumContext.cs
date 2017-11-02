2a61b340ecbe63adbfe994c8d474ae14898b0c95
DenisID <f40f41@gmail.com>
2017-11-02 12:44:46 +0700

    pa.s.Model : enable auto migrations

--- PhotoAlbum.Server.Model/Data/PhotoAlbumContext.cs
+++ PhotoAlbum.Server.Model/Data/PhotoAlbumContext.cs
@@ -13,12 +13,19 @@ namespace PhotoAlbum.Server.Model.Data
         public PhotoAlbumContext()
             : base("name=DefaultConnection")
         {
-            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PhotoAlbumContext>());
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<PhotoAlbumContext>());
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<PhotoAlbumContext, Migrations.Configuration>("DefaultConnection"));
         }
 
         public DbSet<Photo> Photos { get; set; }
         public DbSet<PhotoContent> PhotoContents { get; set; }
         public DbSet<User> Users { get; set; }
         public DbSet<PhotoVote> PhotoVotes { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
