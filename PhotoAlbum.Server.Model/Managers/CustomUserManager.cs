fcc5b728554b5ec6e68a1df0b31de0348c762463
DenisID <f40f41@gmail.com>
2017-11-07 21:41:46 +0700

    pa.s.Model : add CustomUserManager class (e)

---
+++ PhotoAlbum.Server.Model/Managers/CustomUserManager.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity;
+using PhotoAlbum.Server.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Server.Model.Managers
+{
+    public class CustomUserManager : UserManager<ApplicationUser>
+    {
+        public CustomUserManager(IUserStore<ApplicationUser> store)
+            : base(store)
+        {
+        }
+    }
+}
