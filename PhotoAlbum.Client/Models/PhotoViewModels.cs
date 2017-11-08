a73bb232f69b271691f91ffbfc0d4d145b7e6bdf
DenisID <f40f41@gmail.com>
2017-11-08 21:16:22 +0700

    pa.c ViewModels : Photo, CreatePhoto, EditPhoto VM now placed in to PhotoViewModels.cs file

--- PhotoAlbum.Client/Models/PhotoViewModels.cs
+++ PhotoAlbum.Client/Models/PhotoViewModels.cs
@@ -14,4 +14,20 @@ namespace PhotoAlbum.Client.Models
         public byte[] Image { get; set; }
         public string ImageMimeType { get; set; }
     }
+
+    public class CreatePhotoViewModel
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public HttpPostedFileBase Image { get; set; }
+    }
+
+    public class EditPhotoViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
 }
