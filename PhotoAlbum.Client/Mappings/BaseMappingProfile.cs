edc9b64597f04c6aeb05c9f809f05820fbb53962
DenisID <f40f41@gmail.com>
2017-11-08 19:56:19 +0700

    pa.c : add AutoMapperConfiguration, BaseMappingProfile classes

---
+++ PhotoAlbum.Client/Mappings/BaseMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PhotoAlbum.Client.Dto;
+using PhotoAlbum.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PhotoAlbum.Client.Mappings
+{
+    public class BaseMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "BaseMappings"; }
+        }
+
+        public BaseMappingProfile()
+        {
+            CreateMap<EditPhotoViewModel, EditPhotoDto>();
+            //CreateMap<Gadget, GadgetViewModel>();
+        }
+    }
+}
