ae1418750b3bc0bd027084279c92b803d1a784b4
DenisID <f40f41@gmail.com>
2017-11-15 23:15:31 +0700

    pa.c Global.asas.cs : configure ClaimTypes

--- PhotoAlbum.Client/Global.asax.cs
+++ PhotoAlbum.Client/Global.asax.cs
@@ -2,7 +2,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -20,6 +22,8 @@ namespace PhotoAlbum.Client
 
             // Autofac and Automapper configurations
             Bootstrapper.Run();
+
+            AntiForgeryConfig.UniqueClaimTypeIdentifier = ClaimTypes.Name;
         }
     }
 }
