8cf03863820ef46b4fdba587c851e5993b88e5fc
DenisID <f40f41@gmail.com>
2017-11-15 17:20:10 +0700

    pa.c Mapping : add LoginViewModel - GetTokenDto

--- PhotoAlbum.Client/Mappings/ViewModelToDtoMappingProfile.cs
+++ PhotoAlbum.Client/Mappings/ViewModelToDtoMappingProfile.cs
@@ -19,6 +19,7 @@ namespace PhotoAlbum.Client.Mappings
         {
             CreateMap<EditPhotoViewModel, EditPhotoDto>();
             CreateMap<RegisterViewModel, RegisterUserDto>();
+            CreateMap<LoginViewModel, GetTokenDto>();
         }
     }
 }
