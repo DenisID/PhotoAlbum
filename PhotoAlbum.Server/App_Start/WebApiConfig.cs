af559b6171b10e5d802a470a9a8bfdd93c6cb1e0
DenisID <f40f41@gmail.com>
2017-11-19 18:01:43 +0700

    pa.s : add API response wrapper

--- PhotoAlbum.Server/App_Start/WebApiConfig.cs
+++ PhotoAlbum.Server/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using PhotoAlbum.Server.Handlers;
 
 namespace PhotoAlbum.Server
 {
@@ -25,6 +26,8 @@ namespace PhotoAlbum.Server
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.MessageHandlers.Add(new ResponseHandler());
         }
     }
 }
