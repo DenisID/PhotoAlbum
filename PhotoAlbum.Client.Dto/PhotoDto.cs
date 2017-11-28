20e4e3d8427e0cee0922855a3b0ff3df2b486da1
DenisID <f40f41@gmail.com>
2017-11-28 18:01:33 +0700

    pa.c Photo models : add AuthorName prop

--- PhotoAlbum.Client.Dto/PhotoDto.cs
+++ PhotoAlbum.Client.Dto/PhotoDto.cs
@@ -12,6 +12,7 @@ namespace PhotoAlbum.Client.Dto
         public string Title { get; set; }
         public string Description { get; set; }
         public DateTime CreationDate { get; set; }
+        public string AuthorName { get; set; }
         //public byte[] Image { get; set; }
         //public string ImageMimeType { get; set; }
     }
