86164ff7c317305d67ffd2a795f5184bb51ee4a1
DenisID <f40f41@gmail.com>
2017-12-04 13:19:25 +0700

    pa.s : wip (AutoMapper)

---
+++ PhotoAlbum.Server.Model/Mappings/DtoToEntitiesMappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Server.Model.Mappings
+{
+    public class DtoToEntitiesMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "DtoToEntitiesMappings"; }
+        }
+
+        public DtoToEntitiesMappingProfile()
+        {
+        }
+    }
+}
---
+++ PhotoAlbum.Server.Model/Mappings/EntitiesToDtoMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using PhotoAlbum.Server.Dto;
+using PhotoAlbum.Server.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhotoAlbum.Server.Model.Mappings
+{
+    public class EntitiesToDtoMappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return "EntitiesToDtoMappings"; }
+        }
+
+        public EntitiesToDtoMappingProfile()
+        {
+            CreateMap<Photo, PhotoDto>().ForMember()
+        }
+    }
+}
